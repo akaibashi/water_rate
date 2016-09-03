52344b9dd43da3ce6f1b9a570d71abfddb9a711f
赤井橋 健 <titou5273@gmail.com>
2016-09-03 22:58:53 +0900

    test

--- get.php
+++ get.php
@@ -1,28 +1,6 @@
 <?php
 
-// phpQueryの読み込み
-require_once("phpQuery-onefile.php");
-
-var_dump("1");
-
-// HTMLの取得
-$doc = phpQuery::newDocumentFile("http://www.tam-tam.co.jp/tipsnote/");
- 
-var_dump("2");
-
-
-foreach ($doc[".l-main"]->find(".entry-box") as $entry){
-  //更新日
-  $date = pq($entry)->find('time')->attr('datetime');
-  //タイトル
-  $h1 = pq($entry)->find('h1')->text();
-  //配列に格納
-  $jsonData[] = ['title' => $h1, 'date' => $date];
-}
-
-var_dump("3");
-
- 
-//json を出力
-header(" Content-Type:application/json; charset=utf-8");
-echo json_encode($jsonData, JSON_UNESCAPED_UNICODE);
+$html = file_get_contents('http://www.ktr.mlit.go.jp/tonedamu/teikyo/realtime/live/fujiwara.html');
+$dom = new DOMDocument();
+@$dom->loadHTML($html);
+$xml = simplexml_import_dom($dom);//解析したXML文字列をオブジェクトに変換します。
