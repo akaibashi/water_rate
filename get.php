cab995e368b0b4b749e28696d453170800df2f67
赤井橋 健 <titou5273@gmail.com>
2016-09-03 23:38:12 +0900

    test

--- get.php
+++ get.php
@@ -1,38 +1,9 @@
 <?php
-
-
-try{
-
-var_dump("1");
-var_dump("2");
-$doc = new DOMDocument();
-$doc->loadHTML("<html><body>Test<br></body></html>");
-
-var_dump("3");
-
-
-echo $doc->saveHTML();
-
-}catch(Exception $e){
-	var_dump($e);
-}
-
-
-/*
-var_dump("1");
-
 $html = file_get_contents('http://www.ktr.mlit.go.jp/tonedamu/teikyo/realtime/live/fujiwara.html');
-var_dump("2");
 
 $dom = new DOMDocument();
 
-var_dump("3");
-
-
 @$dom->loadHTML($html);
-
-var_dump("4");
 
 
 $xml = simplexml_import_dom($dom);//解析したXML文字列をオブジェクトに変換します。
-*/
