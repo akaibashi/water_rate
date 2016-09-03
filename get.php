de3f6f783d5113cf88c45d4cc7228eefffc52e95
赤井橋 健 <titou5273@gmail.com>
2016-09-03 22:46:54 +0900

    add phpquery.

--- get.php
+++ get.php
@@ -1,3 +1,13 @@
 <?php
 
-var_dump("get!!");
+//require
+require_once('phpQuery-onefile.php');
+
+//ページ取得
+$html = file_get_contents('./test.html');
+
+//DOM取得
+$doc = phpQuery::newDocument($html);
+
+//要素取得
+echo $doc["title"]->text();
