3cfdecfbcaeb4aa929cde7b9bfc2312da60b1bfe
赤井橋 健 <titou5273@gmail.com>
2016-09-03 22:55:25 +0900

    test

--- get.php
+++ get.php
@@ -2,9 +2,15 @@
 
 // phpQueryの読み込み
 require_once("phpQuery-onefile.php");
+
+var_dump("1");
+
 // HTMLの取得
 $doc = phpQuery::newDocumentFile("http://www.tam-tam.co.jp/tipsnote/");
  
+var_dump("2");
+
+
 foreach ($doc[".l-main"]->find(".entry-box") as $entry){
   //更新日
   $date = pq($entry)->find('time')->attr('datetime');
@@ -13,6 +19,9 @@ foreach ($doc[".l-main"]->find(".entry-box") as $entry){
   //配列に格納
   $jsonData[] = ['title' => $h1, 'date' => $date];
 }
+
+var_dump("3");
+
  
 //json を出力
 header(" Content-Type:application/json; charset=utf-8");
