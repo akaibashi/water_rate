ba9fef6ffec56636dcde07f1ca66a534159217ea
赤井橋 健 <titou5273@gmail.com>
2016-09-03 23:00:11 +0900

    test

--- get.php
+++ get.php
@@ -1,6 +1,18 @@
 <?php
 
+var_dump("1");
+
 $html = file_get_contents('http://www.ktr.mlit.go.jp/tonedamu/teikyo/realtime/live/fujiwara.html');
+var_dump("2");
+
 $dom = new DOMDocument();
+
+var_dump("3");
+
+
 @$dom->loadHTML($html);
+
+var_dump("4");
+
+
 $xml = simplexml_import_dom($dom);//解析したXML文字列をオブジェクトに変換します。
