96abbdf343d336a751a8822f3c3c630f398a118a
赤井橋 健 <titou5273@gmail.com>
2016-09-03 22:51:08 +0900

    test

--- get.php
+++ get.php
@@ -8,8 +8,18 @@ var_dump("aaa");
 // 藤沢ダム
 //ページ取得
 $html = file_get_contents('http://www.ktr.mlit.go.jp/tonedamu/teikyo/realtime/live/fujiwara.html');
+
+
+var_dump("aa2");
+
 //DOM取得
 $doc = phpQuery::newDocument($html);
 
+var_dump("aa3");
+
+
 //要素取得
 echo $doc["title"]->text();
+
+
+var_dump("aa4");
